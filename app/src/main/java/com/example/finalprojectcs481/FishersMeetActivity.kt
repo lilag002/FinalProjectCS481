2d4f5cc4e1f58add3501edb16e1e0eb9003ee199
Alex <nelso177@csusm.edu>
2023-10-31 16:36:48 -0700

    BIG FRAGMENTS

---
+++ app/src/main/java/com/example/finalprojectcs481/FishersMeetActivity.kt
@@ -0,0 +1,11 @@
+package com.example.finalprojectcs481
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FishersMeetActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fishers_meet)
+    }
+}
