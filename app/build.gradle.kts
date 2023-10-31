d628762a51f5ec6142688753b8fa804b957da31d
Alex <nelso177@csusm.edu>
2023-10-31 14:52:15 -0700

    BIG BNOOTS

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.finalprojectcs481"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.finalprojectcs481"
