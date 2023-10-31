1810fd1b469d314f03c0ef2d576bba3d154fa561
Alex <nelso177@csusm.edu>
2023-10-31 15:45:47 -0700

    BIG ACTIVITIES

--- app/src/main/java/com/example/finalprojectcs481/WelcomeActivity.kt
+++ app/src/main/java/com/example/finalprojectcs481/WelcomeActivity.kt
@@ -3,10 +3,10 @@ package com.example.finalprojectcs481
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_welcome)
     }
     //BIG BNOOts
 }
