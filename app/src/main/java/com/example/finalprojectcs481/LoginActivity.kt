13bc4d03132aeff7b0d4eaa04da0af7004c6508f
nagalil-honaj-hteal3 <elaeth.jonah@gmail.com>
2023-11-04 19:09:40 -0700

    changes added for both login and signup

--- app/src/main/java/com/example/finalprojectcs481/LoginActivity.kt
+++ app/src/main/java/com/example/finalprojectcs481/LoginActivity.kt
@@ -1,11 +1,19 @@
 package com.example.finalprojectcs481
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.widget.Button
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val backButton = findViewById<Button>(R.id.backButton)
+        backButton.setOnClickListener {
+            val intent = Intent(this, WelcomeActivity::class.java)
+            startActivity(intent)
+            finish() // Close the current LoginActivity
+        }
     }
 }
--- app/src/main/java/com/example/finalprojectcs481/SignupActivity.kt
+++ app/src/main/java/com/example/finalprojectcs481/SignupActivity.kt
@@ -1,11 +1,20 @@
 package com.example.finalprojectcs481
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SignupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+        val backButton = findViewById<Button>(R.id.backButton)
+        backButton.setOnClickListener {
+            val intent = Intent(this, WelcomeActivity::class.java)
+            startActivity(intent)
+            finish() // Close the current LoginActivity
+        }
     }
 }
