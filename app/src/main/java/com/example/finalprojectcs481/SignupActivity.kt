e07fba81b9381ee3be666bd1434f38dfde8c856e
Alex <nelso177@csusm.edu>
2023-11-04 22:10:54 -0700

    BIG EMAIL CHECKER

--- app/src/main/java/com/example/finalprojectcs481/SignupActivity.kt
+++ app/src/main/java/com/example/finalprojectcs481/SignupActivity.kt
@@ -3,7 +3,14 @@ package com.example.finalprojectcs481
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.util.Patterns
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.example.finalprojectcs481.fragments.HomeFragment
+import java.util.regex.Pattern
+
 
 class SignupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,10 +18,40 @@ class SignupActivity : AppCompatActivity() {
         setContentView(R.layout.activity_signup)
 
         val backButton = findViewById<Button>(R.id.backButton)
+        val signupButton = findViewById<Button>(R.id.submit_button)
+
         backButton.setOnClickListener {
             val intent = Intent(this, WelcomeActivity::class.java)
             startActivity(intent)
             finish() // Close the current LoginActivity
+        }
+
+//        val emailPattern = Pattern.compile(
+//            "[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}\\@[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}(\\.[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25})+"
+//        )
+        val email = findViewById<EditText>(R.id.editTextTextEmailAddress).text
+
+        fun isValidString(str: String): Boolean{
+            return Patterns.EMAIL_ADDRESS.matcher(str).matches()
+        }
+
+        signupButton.setOnClickListener {
+            if (!isValidString(email.toString())) {
+                Toast.makeText(
+                this@SignupActivity,
+                "invalid email format $email!",
+                Toast.LENGTH_SHORT
+            ).show()
+                return@setOnClickListener
+            }
+            Toast.makeText(
+                this@SignupActivity,
+                "successfully signed up $email!",
+                Toast.LENGTH_SHORT
+            ).show()
+            val intent = Intent(this, WelcomeActivity::class.java)
+            startActivity(intent) // Close the current Activity
+
         }
     }
 }
