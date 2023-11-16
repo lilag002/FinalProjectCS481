9f7bf0d77bfaccbb57517baef6b14d0deaefa672
Christian Manibusan <cam007mani@gmail.com>
2023-11-15 23:47:22 -0800

    init

---
+++ app/src/main/java/com/example/finalprojectcs481/postModelData/PostData.kt
@@ -0,0 +1,8 @@
+package com.example.finalprojectcs481.postModelData
+
+data class PostData(
+    // probably store other post information in here
+    val title: String,
+    val userName: String,
+    val image: Int
+)
---
+++ app/src/main/java/com/example/finalprojectcs481/recyclerViewHome/RVAdapterHome.kt
@@ -0,0 +1,56 @@
+package com.example.finalprojectcs481.recyclerViewHome
+
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.finalprojectcs481.R
+import com.example.finalprojectcs481.postModelData.PostData
+
+
+    class RVAdapterHome(private val postList: List<PostData>): RecyclerView.Adapter<RVAdapterHome.ViewHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.recycler_view_home_row,parent,false)
+            return ViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            val item = postList[position]
+//            holder.tvID.text = "ID: "+item.id.toString()
+//            holder.tvUSERID.text = "userID: "+item.userId.toString()
+//            holder.tvTITLE.text = "Title: "+item.title.toString()
+//            holder.tvCOMPLETED.text = "Completed: "+item.completed.toString()
+            holder.tvTITLE.text = item.title.toString()
+            holder.tvUSERNAME.text = item.userName.toString()
+            holder.ivPOST.id = item.image
+
+        }
+
+        override fun getItemCount(): Int {
+            return postList.size
+        }
+
+        inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+//            val tvID: TextView = itemView.findViewById(R.id.tvid);
+//            val tvUSERID: TextView = itemView.findViewById(R.id.tvuserid);
+//            val tvTITLE: TextView = itemView.findViewById(R.id.tvtitle);
+//            val tvCOMPLETED: TextView = itemView.findViewById(R.id.tvcompleted)
+            val tvTITLE: TextView = itemView.findViewById(R.id.textViewPostTitle)
+            val tvUSERNAME: TextView = itemView.findViewById(R.id.textViewPostUsername)
+            val ivPOST: ImageView = itemView.findViewById(R.id.imageViewPost)
+//            init {
+//                itemView.setOnClickListener{
+//                    val intent = Intent(itemView.context, ViewActivity::class.java)
+//                    intent.putExtra("userId",dataList[adapterPosition].userId.toString())
+//                    intent.putExtra("id",dataList[adapterPosition].id.toString())
+//                    intent.putExtra("title",dataList[adapterPosition].title)
+//                    intent.putExtra("cid",dataList[adapterPosition].cid)
+//                    itemView.context.startActivity(intent)
+//                }
+//            }
+        }
+    }
