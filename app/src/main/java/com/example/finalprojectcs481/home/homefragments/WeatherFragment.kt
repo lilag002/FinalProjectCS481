7d8b3977bdbb35fa05690e0605312b1252972109
nagalil-honaj-hteal3 <elaeth.jonah@gmail.com>
2023-11-25 19:33:22 -0800

    added conversion, type of forecast, improved output statement

--- app/src/main/java/com/example/finalprojectcs481/home/homefragments/WeatherFragment.kt
+++ app/src/main/java/com/example/finalprojectcs481/home/homefragments/WeatherFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.Spinner
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
@@ -38,6 +40,8 @@ class WeatherFragment : Fragment() {
         var cityNameFilled = false
         cityName = view.findViewById<EditText>(R.id.cityTextView)
 
+        val loadingIndicator = view.findViewById<ProgressBar>(R.id.loadingIndicator)
+
         // Determine whether it's day or night
         val isDayTime = currentHour in 6..16
 
@@ -51,7 +55,12 @@ class WeatherFragment : Fragment() {
         )
         // Initialize and execute the task to fetch weather data (coroutine)
         view.findViewById<Button>(R.id.submitBtn).setOnClickListener {
+            loadingIndicator.visibility = View.VISIBLE
+
             val enteredCity = cityName.text.toString().trim()
+            val unitSpinner = view.findViewById<Spinner>(R.id.unitSpinner)
+            val selectedUnit = unitSpinner.selectedItem.toString()
+
             if (enteredCity.isNotEmpty()) {
                 // Call fetchweather funct when the button is clicked and cityName is not empty
                 // *runs second coroutine due to coroutine funct needing to be inside coroutine body*
@@ -60,7 +69,7 @@ class WeatherFragment : Fragment() {
                         val result = fetchWeatherData(enteredCity)
                         result?.let { json ->
                             // Update your UI with the weather data
-                            updateUI(json)
+                            updateUI(json, selectedUnit)
                         }
                     } catch (e: Exception) {
                         // Handle exceptions (e.g., network errors)
@@ -69,7 +78,7 @@ class WeatherFragment : Fragment() {
                     }
                 }
             } else {
-                Toast.makeText(context, "Empty city name", Toast.LENGTH_SHORT).show()
+                Toast.makeText(context, "Empty city name. Please enter again", Toast.LENGTH_SHORT).show()
             }
         }
     }
@@ -104,20 +113,41 @@ class WeatherFragment : Fragment() {
         }
     }
 
-    private fun updateUI(json: JSONObject) {
+    private fun updateUI(json: JSONObject, selectedUnit: String) {
         try {
-            // Example: Extract temperature from the JSON response
+            // Extract temperature from the JSON response
             val main = json.getJSONObject("main")
             val temperatureValue = main.getDouble("temp")
 
-            // Example: Update a TextView with the temperature
+            // Extract city name from the JSON response
+            val cityName = json.getString("name")
+
+            // Extract weather information from the JSON response
+            val weatherArray = json.getJSONArray("weather")
+            val weatherObject = weatherArray.getJSONObject(0) // Assuming the first item contains the main weather condition
+            val weatherCondition = weatherObject.getString("main")
+
+            // Example: Update a TextView with the temperature and city name
             val temperatureTextView = view?.findViewById<TextView>(R.id.temperatureTextView)
+            val weatherConditionTextView = view?.findViewById<TextView>(R.id.weatherConditionTextView)
+
+            // Convert temperature based on the selected unit
+            val convertedTemperature = if (selectedUnit == "Celsius") {
+                // Convert to Celsius and round to two decimal places
+                String.format("%.2f", (temperatureValue - 32) * 5 / 9).toDouble()
+            } else {
+                // Keep it in Fahrenheit and round to two decimal places
+                String.format("%.2f", temperatureValue).toDouble()
+            }
 
             // Convert Double to String
-            val temperatureString = temperatureValue.toString()
+            val temperatureString = "Today's temperature in $cityName is $convertedTemperature°$selectedUnit"
 
             // Set the value in the TextView
             temperatureTextView?.text = temperatureString
+
+            // Display the weather condition
+            weatherConditionTextView?.text = "Weather: $weatherCondition"
 
             // Continue extracting and updating other relevant information
             // ...
@@ -126,4 +156,6 @@ class WeatherFragment : Fragment() {
             e.printStackTrace()
         }
     }
+
+
 }
